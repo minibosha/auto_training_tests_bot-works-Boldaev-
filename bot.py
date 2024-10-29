9b3a8d9b9f1a5c82aeb7632c3cbb7e1c3dff8fb9
user10w <minibosha@gmail.com>
2024-10-29 18:58:41 +0300

    test_1010

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ import telebot
 from dotenv import load_dotenv
 from os import getenv
 
+import requests
+
+
 # Подгрузка токена бота
 load_dotenv()
 token = getenv('token')
@@ -14,12 +17,15 @@ bot = telebot.TeleBot(token, parse_mode=None)
 # Команды бота и ответы на них
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Bot not ready yet... \nwait for full version)))")
 
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(command=['games'])
+def send_games(message):
+    url = 'https://search.nintendo-europe.com/en/select?fq=type%3AGAME+AND+system_type%3Anintendoswitch*+AND+product_code_txt%3A*&q=*&sort=change_date+desc&start=0&wt=json&rows=10'
+    data = requests.get(url).json()
+    response = '\n'.join([game['title'] for game in data['response']['docs']])
+    bot.send_message(message.chat.id, response)
 
 
 # Запуск бота
